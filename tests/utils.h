82e9ef5a3caa7ff7273f59d37f20d4d166686e2d
gatepara <wtkj@wtblog.cn>
2024-08-10 23:19:27 +0800

    fix bug

--- tests/utils.h
+++ tests/utils.h
@@ -15,8 +15,8 @@ void load_data(char *filename, float *&data, unsigned &num, unsigned &dim)
     in.seekg(0, std::ios::end);
     std::ios::pos_type ss = in.tellg();
     size_t fsize = (size_t)ss;
-    num = (unsigned)(fsize / (dim + 1) / 4);
-    data = new float[num * dim * sizeof(float)];
+    num = (size_t)(fsize / (dim + 1) / 4);
+    data = new float[num * dim];
 
     in.seekg(0, std::ios::beg);
     for (size_t i = 0; i < num; i++)
